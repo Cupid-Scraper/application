081e54d60cf6fc445afadc6f66910f5f716d780d
Lawerence Lee <lucifitz.edward@gmail.com>
2017-11-25 16:30:59 -0800

    renamed details section to background, moved what used to be details and misc details out of essay section.

--- config_scraper.py
+++ config_scraper.py
@@ -10,8 +10,11 @@ PLEASE KEEP THIS FILE SAFE AND GRANT ONLY YOUR SELF READ/WRITE PRIVLEDGES.
 USERNAME = 'EdwardLucifitz'
 PASSWORD = 'r2V-TZK-dX6-Dfc'
 
-# LOCATIONS where you'd like to find a match (location spelling needs to match
-# the spelling on OkCupid)
+# ****************************************************************************
+# ALL STRINGS BELOW THIS TEXT SHOULD MATCH OKCUPID'S SPELLING (INCLUDING CAPS)
+# ****************************************************************************
+
+# LOCATIONS where you'd like to find a match ()
 locations = [
     'San Francisco', 'Berkeley', 'Oakland', 'Albany', 'Emeryville', 'Piedmont',
     'El Cerrito', 'Alameda',
@@ -19,6 +22,29 @@ locations = [
 
 # FLAGS ALL MATCHES AT OR ABOVE THE GIVEN %
 percentage = 80
+
+# Details
+background_wanted = [
+    'Atheist', 'Agnostic'
+]
+
+# Details Not Wanted (Just flags you that the word/s exist in the details
+# section)
+background_not_wanted = [
+    'Christian', 'Catholic', 'Muslim', 'Buddhist', 'Hindu', 'Jewish', 'Sikh',
+    'Other',
+]
+
+# Mics Details
+misc_details_wanted = [
+
+]
+
+# Misc Details Not Wanted (Just flags you that the word/s exist in the misc
+# details section)
+misc_details_not_wanted = [
+    'Vegan', 'Vegetarian',
+]
 
 
 # ****************************************************************************
@@ -61,32 +87,10 @@ adjectives = [
     'musician', 'pianist', 'hilarious',
 ]
 
-# Absolutely Not - Matches that have these words in their profile will
+# Absolutely Not - Matches that have these words in their essays will
 # automatically be HIDDEN. Be careful with this one.
 no_way = [
 
-]
-# -----------------------------------------------------------------------------
-
-
-# Details
-details_wanted = [
-
-]
-
-# Details Not Wanted
-details_not_wanted = [
-
-]
-
-# Mics Details
-misc_details_wanted = [
-
-]
-
-# Misc Details Not Wanted
-misc_details_not_wanted = [
-    'vegan', 'vegetarian',
 ]
 
 
@@ -121,7 +125,7 @@ music = [
     'john williams', 'grateful dead', 'herbie hancock', 'miles davis',
     'eric clapton', 'devo', 'thin lizzy', 'rush', 'al green', 'black sabbath',
     'bruce springsteen', 'funkadelic', 'black keys', 'the cars', 'the beatles',
-    'bach', 'tom petty', 'heartbreakers', 'incredible bongo band',
+    'bach', 'tom petty', 'heartbreakers', 'incredible bongo band', 'kinks',
     'allman brothers band', 'rolling stones', 'led zeppelin',
     'billy joel', 'beach boys', 'boston', 'loggins & messina',
     'tom waits', 'queen', 'cheap trick', 'doobie brothers',
