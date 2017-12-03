b412e78df1a638fbf546e32dd3c676ea4370de03
Lawerence Lee <lucifitz.edward@gmail.com>
2017-12-02 23:04:54 -0800

    turned off peewee, changed config_lists into a dictionary,  added background to bkground in check_background(),  added check_essay(), checks five matches at a time.

--- cupid_scraper.py
+++ cupid_scraper.py
@@ -5,7 +5,7 @@ from selenium.common.exceptions import (NoSuchElementException,
 from selenium import webdriver
 
 import config_scraper
-from models import Person, initialize, ViewedPerson
+# from models import Person, initialize,
 
 WARNING_MESSAGE_ON = False
 BROWSER = webdriver.Firefox()
@@ -30,18 +30,30 @@ ATTR_XPATH = {
     '//*[@id="react-profile-wiw"]/div[2]',
 }
 
-config_lists = [
-    config_scraper.activities, config_scraper.education,
-    config_scraper.topics_interest, config_scraper.politics,
-    config_scraper.adjectives, config_scraper.no_way,
-    config_scraper.authors, config_scraper.books,
-    config_scraper.comedians, config_scraper.food,
-    config_scraper.directors, config_scraper.movies,
-    config_scraper.music, config_scraper.music_genres,
-    config_scraper.news_mags, config_scraper.radio,
-    config_scraper.television, config_scraper.youtube,
-    config_scraper.your_six_things, config_scraper.your_thoughts
-]
+config_lists = {
+    'activities': config_scraper.activities,
+    'education': config_scraper.education,
+    'topics_interest': config_scraper.topics_interest,
+    'politics': config_scraper.politics,
+    'adjectives': config_scraper.adjectives,
+    'no_way': config_scraper.no_way,
+    'authors': config_scraper.authors,
+    'books': config_scraper.books,
+    'comedians': config_scraper.comedians,
+    'food': config_scraper.food,
+    'directors': config_scraper.directors,
+    'movies': config_scraper.movies,
+    'music': config_scraper.music,
+    'music_genres': config_scraper.music_genres,
+    'news_mags': config_scraper.news_mags,
+    'radio': config_scraper.radio,
+    'television': config_scraper.television,
+    'youtube': config_scraper.youtube,
+    'your_six_things': config_scraper.your_six_things,
+    'your_thoughts': config_scraper.your_thoughts,
+    'general_words':
+    config_scraper.general_words,
+}
 
 WARNING_MESSAGE = """
 UPON FILLING OUT 'config_scraper.py' BE WARY THAT THIS FILE NOW CONTAINS
@@ -91,13 +103,9 @@ def grab_match_cards(browser):
 def grab_match_links(match_cards):
     match_links = []
     for card in match_cards:
-        name = card.find_element_by_class_name('name')
-        if not ViewedPerson.select().where(
-                                ViewedPerson.name == name.text).exists():
-            ViewedPerson.create_viewed_person(name.text)
-            link = card.find_element_by_class_name('name')
-            link = link.get_attribute("href")
-            match_links.append(link)
+        link = card.find_element_by_class_name('name')
+        link = link.get_attribute("href")
+        match_links.append(link)
 
     print('[+] Found {} Matches'.format(len(match_links)))
     return match_links
@@ -140,10 +148,6 @@ def get_profile_attrs(browser, link):
     essays = browser.find_elements_by_class_name("essays2015-essay-content")
     essay_list = zip(essay_titles, essays)
 
-    # for title, essay in essay_list:
-    #     print(title.text)
-    #     print(essay.text, '\n\n')
-
     return (username, age, location, percentage,
             basics, background, misc_details,
             looking_for, essay_list, link)
@@ -151,7 +155,8 @@ def get_profile_attrs(browser, link):
 
 def parse_profile_attrs(attributes):
     print('\n')
-    print(attributes[0])
+    print('USERNAME: {}.'.format(attributes[0]))
+    print('WEB ADDRESS: {}'.format(attributes[-1]))
 
     check_age(attributes[1], config_scraper.age_range)
     check_location(attributes[2], config_scraper.locations)
@@ -171,8 +176,9 @@ def parse_profile_attrs(attributes):
         config_scraper.misc_details_wanted,
         config_scraper.misc_details_not_wanted
     )
-
-    # check_self_summary()
+    check_essays(
+        attributes[8]
+    )
 
 
 def check_age(age, age_range):
@@ -196,6 +202,9 @@ def check_percentage(match_percentage, pref_percent):
         print(
             "[+] Matched Percentage Range: {}%".format(
                 match_percentage[0]))
+    else:
+        print('    [-] Did Not Match Percentage Range: {}%'.format(
+            match_percentage[0]))
 
 
 def check_basics(basics, basics_wanted, basics_not_wanted):
@@ -212,30 +221,29 @@ def check_basics(basics, basics_wanted, basics_not_wanted):
         print('[+] Matched Wanted basics: {}'.format(', '.join(
             matched_wanted_basics)))
     if matched_unwanted_basics:
-        print('[-] Matched Unwanted basics: {}'.format(', '.join(
+        print('    [-] Matched Unwanted basics: {}'.format(', '.join(
             matched_unwanted_basics)))
 
 
-def check_background(background, background_wanted, background_not_wanted):
-    background = background.split(', ')
-    matched_wanted_background = []
-    matched_unwanted_background = []
-    for detail in background:
-        if detail.strip() in background_wanted:
-            matched_wanted_background.append(detail)
-        if detail.strip() in background_not_wanted:
-            matched_unwanted_background.append(detail)
+def check_background(bkground, bkground_wanted, bkground_not_wanted):
+    bkground = bkground.split(', ')
+    matched_wanted_bkground = []
+    matched_unwanted_bkground = []
+    for detail in bkground:
+        if detail.strip() in bkground_wanted:
+            matched_wanted_bkground.append(detail)
+        if detail.strip() in bkground_not_wanted:
+            matched_unwanted_bkground.append(detail)
 
-    if matched_wanted_background:
+    if matched_wanted_bkground:
         print('[+] Matched Wanted background: {}'.format(', '.join(
-            matched_wanted_background)))
-    if matched_unwanted_background:
-        print('[-] Matched Unwanted background: {}'.format(', '.join(
-            matched_unwanted_background)))
+            matched_wanted_bkground)))
+    if matched_unwanted_bkground:
+        print('    [-] Matched Unwanted background: {}'.format(', '.join(
+            matched_unwanted_bkground)))
 
 
-def check_misc_details(misc_details,
-                       misc_details_wanted,
+def check_misc_details(misc_details, misc_details_wanted,
                        misc_details_not_wanted):
     misc_details = misc_details.split(', ')
     matched_wanted_misc_details = []
@@ -250,8 +258,30 @@ def check_misc_details(misc_details,
         print('[+] Matched Wanted Misc Details: {}'.format(', '.join(
             matched_wanted_misc_details)))
     if matched_unwanted_misc_details:
-        print('[-] Matched Unwanted Misc Details: {}'.format(', '.join(
+        print('    [-] Matched Unwanted Misc Details: {}'.format(', '.join(
             matched_unwanted_misc_details)))
+
+
+def check_essays(essays):
+    print("\nChecking essays against 'config_scraper'...\n")
+    for essay_name, essay_paragraph in essays:
+        counter = 0
+        for lst_name, values in config_lists.items():
+            values_present = []
+            for item in values:
+                if item in essay_paragraph.text:
+                    values_present.append(item)
+                elif item.upper() in essay_paragraph.text:
+                    values_present.append(item)
+                elif item.title() in essay_paragraph.text:
+                    values_present.append(item)
+            if values_present:
+                if counter == 0:
+                    print('\n> > > {}'.format(essay_name.text)) 
+                print('[+] Matched Items in {} list:'.format(
+                                                lst_name.upper()))
+                for value in values_present:
+                    print('   {}'.format(value))
 
 
 def main():
@@ -262,22 +292,25 @@ def main():
     print('<3'*5, 'CUPID SCRAPER', '<3'*5)
     if WARNING_MESSAGE_ON is False:
         print('[*] WARNING MESSAGE DISABLED')
+
     sign_in(BROWSER)
     match_cards = grab_match_cards(BROWSER)
     match_links = grab_match_links(match_cards)
 
-    # for link in match_links:
-    #     attributes = get_profile_attrs(BROWSER, link)
-
-    attributes = get_profile_attrs(BROWSER, match_links[0])
-    parse_profile_attrs(attributes)
-
-    BROWSER.quit()
+    counter = 0
+    for link in match_links[:4]:
+        if counter > 0:
+            input('\n***** Press ENTER to search next match *****\n\n')
+        attributes = get_profile_attrs(BROWSER, link)
+        parse_profile_attrs(attributes)
+        counter += 1
+    
+    # BROWSER.quit()
     print('\n[+] Closed Browser')
     print('<3'*17, '\n')
 
 
 if __name__ == "__main__":
     clear()
-    initialize()
+    # initialize()
     main()
