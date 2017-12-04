a572a3f31ab0a4edced6058fe55e9288ff20baa9
Lawerence Lee <lucifitz.edward@gmail.com>
2017-12-04 14:22:21 -0800

    inital commit

---
+++ template_config_scraper.py
@@ -0,0 +1,177 @@
+#!/usr/bin/python3
+"""Includes the various lists and dictionaries need to configure your
+personal OkCupid Scraper.
+
+PLEASE KEEP THIS FILE SAFE AND GRANT ONLY YOUR SELF READ/WRITE PRIVLEDGES.
+"""
+
+
+# OkCupid Credentials
+USERNAME = ''
+PASSWORD = ''
+
+# ****************************************************************************
+# ALL STRINGS BELOW THIS TEXT SHOULD MATCH OKCUPID'S SPELLING (INCLUDING CAPS)
+# ****************************************************************************
+
+# AGE Range Perference (Must use integers)
+# You may specify either value, both values, or neither.
+greater_than_or_equal_to = None
+less_than_or_equal_to = None
+age_range = [
+    greater_than_or_equal_to,
+    less_than_or_equal_to
+]
+
+# LOCATIONS where you'd like to find a match ()
+locations = [
+
+]
+
+# FLAGS ALL MATCHES AT OR ABOVE THE GIVEN %
+percentage = 80
+
+# Details
+basics_wanted = [
+
+]
+# Details Not Wanted
+basics_not_wanted = [
+
+]
+
+# Background
+background_wanted = [
+
+]
+
+# Details Not Wanted (Just flags you that the word/s exist in the details
+# section)
+background_not_wanted = [
+
+]
+
+
+# Mics Details
+misc_details_wanted = [
+
+]
+
+# Misc Details Not Wanted (Just flags you that the word/s exist in the misc
+# details section)
+misc_details_not_wanted = [
+
+]
+
+
+# ****************************************************************************
+# ************ ALL STRINGS BELOW THIS TEXT SHOULD BE LOWER CASE. *************
+# ****************************************************************************
+
+# SEARCHES ALL ESSAYS ---------------------------------------------------
+
+
+activities = [
+
+]
+
+# Education - Place your schools here
+education = [
+
+]
+
+# topics of interest to you
+topics_interest = [
+
+]
+
+politics = [
+
+]
+
+# Searches for specific words you'd like to be in your
+# match's essay sections.
+adjectives = [
+
+]
+
+# Absolutely Not - Matches that have these words in their essays will
+# automatically be HIDDEN. Be careful with this one.
+no_way = [
+
+]
+
+
+# FAVORITES SECTION -----------------------------------------------------------
+
+authors = [
+
+]
+
+books = [
+
+]
+
+comedians = [
+
+]
+
+food = [
+
+]
+
+directors = [
+
+]
+
+actors = [
+
+]
+movies = [
+
+]
+
+# This should just be musical artists.
+music = [
+
+]
+
+music_genres = [
+
+]
+
+news_mags = [
+
+]
+
+radio = [
+
+]
+
+television = [
+
+]
+
+youtube = [
+
+]
+# --------------------------------------------------------------------
+
+
+# SIX THINGS YOU COULD NEVER LIVE WITHOUT
+your_six_things = [
+
+]
+
+# YOU SPEND A LOT OF TIME THINK ABOUT
+your_thoughts = [
+
+]
+
+doin_w_life = [
+
+]
+
+general_words = [
+
+]
